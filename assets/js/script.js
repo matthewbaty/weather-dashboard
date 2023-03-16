672aa2fcc59be75bdebe5675c00db60697349bed
Matthew Baty <mattbaty@outlook.com>
2023-03-15 20:55:33 -0400

    added some vars

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,12 @@
 // jquery
 $(function () {
+    // define variables for API endpoint and API key
+    var apiUrl = "https://api.openweathermap.org/data/2.5/";
+    var apiKey = "87f4db31ceb8c0cd0e7db8203ba43945";
+
+    // search input and search button
+    var searchInput = $("#search-input");
+    var searchButton = $("#search-button");
     
     fetch("https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={87f4db31ceb8c0cd0e7db8203ba43945}")
 });
