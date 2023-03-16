e952f310d25e7f49915a7a9533122d8bbd7f92b7
Matthew Baty <mattbaty@outlook.com>
2023-03-15 21:08:18 -0400

    started weather API function for current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,27 @@ $(function () {
     // search input and search button
     var searchInput = $("#search-input");
     var searchButton = $("#search-button");
-    
-    fetch("https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={87f4db31ceb8c0cd0e7db8203ba43945}")
+
+    $('form').submit(function(event) {
+        // validation
+        event.preventDefault(); 
+        
+        // get the user input 
+        var city = $('input').val().trim();
+        
+        // use the weather API to get the weather data
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=87f4db31ceb8c0cd0e7db8203ba43945`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            // update the page to show info
+            $('.temp').text(`Temperature: ${data.main.temp} K`);
+            $('.wind').text(`Wind: ${data.wind.speed} m/s`);
+            $('.humidity').text(`Humidity: ${data.main.humidity}%`);
+        })
+        .catch(error => console.error(error));
+    });
 });
 
+
+fetch("https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={87f4db31ceb8c0cd0e7db8203ba43945}")
